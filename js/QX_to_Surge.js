cb418ec94a824e06be157f4562756c90b0ecfe98
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-02-07 01:13:45 +0800

    更新 QX_to_Surge.js

--- js/QX_to_Surge.js
+++ js/QX_to_Surge.js
@@ -236,8 +236,8 @@ ${MapLocal}
 
 
 ${MITM}`
-		.replace(/(#.+\n)\n/g,'$1')
 		.replace(/t&zd;/g,',')
+		.replace(/(#.+\n)\n+/g,'$1')
 		.replace(/\n{2,}/g,'\n\n')
 
 if (isSurgeiOS || isStashiOS) {
