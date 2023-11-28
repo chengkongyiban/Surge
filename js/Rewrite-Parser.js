72de881461bf45261178fb844b7d9c8781c073b2
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-11-28 11:45:13 +0800

    更新 Rewrite-Parser.js

--- js/Rewrite-Parser.js
+++ js/Rewrite-Parser.js
@@ -421,7 +421,7 @@ if (/url +echo-response | data *= *"/.test(x)) getMockInfo(x);
 
 inBox.length != 0 && ntf == true && $.msg('Script Hub: 重写转换','已根据关键词保留以下内容',`${inBox}`);
 outBox.length != 0 && ntf == true && $.msg('Script Hub: 重写转换','已根据关键词排除以下内容',`${outBox}`);
-$.log($.toStr(ruleBox))
+
 //mitm删除主机名
 if (hnDel != null && hnBox.length != 0) hnBox=hnBox.filter(function(item) {
     return hnDel.indexOf(item) == -1
